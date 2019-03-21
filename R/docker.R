0b2a0ff9c1cc316a67338fce400d261e3699f9f8
C. T. Lente <ctlente@users.noreply.github.com>
2019-03-21 15:13:06 -0300

    Gcloud and docker

--- R/docker.R
+++ R/docker.R
@@ -1,4 +1,9 @@
 
+#' Check whether current user has docker setup
+#'
+#' @description This function checks two things: if the curent user has docker
+#' installed (via the `which docker` shell command) and if docker can be run
+#' without sudo privileges (by testing the `docker info` command).
 has_docker <- function() {
 
   docker <- sys("which docker", intern = TRUE, ignore.stderr = TRUE)$result
@@ -10,6 +15,22 @@ has_docker <- function() {
   return(TRUE)
 }
 
+#' Install docker for the current user
+#'
+#' @description This function can run two routines: full docker installation
+#' if the user doesn't have it or correct privilege setup if the user runs
+#' docker with sudo. The first method fetches and runs the script located
+#' at \url{https://get.docker.com} and the second only runs
+#' `sudo usermod -aG docker $USER`.
+#'
+#' @section Sudo:
+#' To install docker, this function requires sudo privileges. To do that, it
+#' prompts the user for their password and maintains it until the end of
+#' execution. All commands run this way are **printed** to the console so
+#' the user can know everything that's happening.
+#'
+#' @return If everything has gone as expected, `TRUE`.
+#' @export
 install_docker <- function() {
 
   docker <- sys("which docker", intern = TRUE, ignore.stderr = TRUE)$result
@@ -45,4 +66,3 @@ install_docker <- function() {
 
   return(TRUE)
 }
-
--- R/gcloud.R
+++ R/gcloud.R
@@ -1,4 +1,8 @@
 
+#' Check whether current user has gcloud setup
+#'
+#' @description This function checks if the curent user has gcloud installed
+#' (via the `which gcloud` shell command).
 has_gcloud <- function() {
 
   gcloud <- sys("which gcloud", intern = TRUE, ignore.stderr = TRUE)$result
@@ -7,6 +11,20 @@ has_gcloud <- function() {
   return(TRUE)
 }
 
+#' Install gcloud for the current user
+#'
+#' @description This function runs the gcloud installation routine via
+#' `apt-get install google-cloud-sdk`. It also installs the `kubectl`
+#' package for Kubernetes functionality.
+#'
+#' @section Sudo:
+#' To install gcloud, this function requires sudo privileges. To do that, it
+#' prompts the user for their password and maintains it until the end of
+#' execution. All commands run this way are **printed** to the console so
+#' the user can know everything that's happening.
+#'
+#' @return If everything has gone as expected, `TRUE`.
+#' @export
 install_gcloud <- function() {
 
   gcloud <- sys("which gcloud", intern = TRUE, ignore.stderr = TRUE)$result
@@ -31,3 +49,35 @@ install_gcloud <- function() {
   return(TRUE)
 }
 
+#' Set gcloud configuration
+#'
+#' @description Sets gcloud's current project and zone configs by running
+#' `gcloud config set project` and `gcloud config set compute/zone`.
+#'
+#' @param project The desired `[PROJECT_ID]` (or `NULL` to skip)
+#' @param zone The desired `[COMPUTE_ZONE]` (or `NULL` to skip)
+#' @param region The desired `[COMPUTE_REGION]` (or `NULL` to skip)
+#'
+#' @return If everything has gone as expected, `TRUE`.
+#' @export
+gcloud_set_config <- function(project = NULL, zone = NULL, region = NULL) {
+  if (!is.null(project)) { print_run(paste0("gcloud config set project ", project)) }
+  if (!is.null(zone)) { print_run(paste0("gcloud config set compute/zone ", zone)) }
+  if (!is.null(region)) { print_run(paste0("gcloud config set compute/region ", region)) }
+
+  return(TRUE)
+}
+
+#' Get gcloud configuration
+#'
+#' @description Prints to the console gcloud's current configuration by running
+#' the `gcloud config list` command.
+#'
+#' @return The string vector returned by the command (invisibly).
+#' @export
+gcloud_get_config <- function() {
+  out <- system("gcloud config list", intern = TRUE, ignore.stderr = TRUE)
+  cat(out, sep = "\n")
+
+  invisible(out)
+}
